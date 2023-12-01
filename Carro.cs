9e65ce75b5650233898811e20ff6f4b1ce553b03
Peter090304 <aguirrepedro206@gmail.com>
2023-12-01 10:50:00 -0700

    Agregar archivos de proyecto.

---
+++ Carro.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuperacion_PHAC
+{
+    class Carro
+    {
+        public int Carros { get; set; }
+        public string Modelo { get; set; }
+        public int Año { get; set; }
+
+
+        public Carro(int carro, string modelo, int año)
+        {
+            Carros = carro;
+            Modelo = modelo;
+            Año = año;
+
+        }
+    }
+
+    class Nodo
+    {
+        public Carro Datos { get; set; }
+        public Nodo Siguiente { get; set; }
+    }
+
+    class ListaEnlazada
+    {
+        private Nodo cabeza;
+
+        public void AgregarCarro(Carro carro)
+        {
+            Nodo nuevoNodo = new Nodo { Datos = carro, Siguiente = null };
+
+            if (cabeza == null)
+            {
+                cabeza = nuevoNodo;
+            }
+            else
+            {
+                Nodo actual = cabeza;
+                while (actual.Siguiente != null)
+                {
+                    actual = actual.Siguiente;
+                }
+                actual.Siguiente = nuevoNodo;
+            }
+        }
+
+
+        public int EncontrarCarro(string nombre)
+        {
+            Nodo actual = cabeza;
+            int posicion = 0;
+
+            while (actual != null)
+            {
+                if (actual.Datos.Modelo == nombre)
+                {
+                    return posicion;
+                }
+
+                actual = actual.Siguiente;
+                posicion++;
+            }
+
+            return -1;
+        }
+
+        public void Imprimirporaño()
+        {
+            OrdenarPorAño();
+            Nodo actual = cabeza;
+
+            while (actual != null)
+            {
+                Console.WriteLine($"Nombre:{actual.Datos.Modelo},Año:{actual.Datos.Año}");
+                actual = actual.Siguiente;
+            }
+        }
+
+        public void OrdenarPorAño()
+        {
+            if (cabeza == null || cabeza.Siguiente == null)
+            {
+                return;
+            }
+
+            Nodo actual = cabeza;
+
+            while (actual != null)
+            {
+                Nodo siguiente = actual.Siguiente;
+
+                while (siguiente != null)
+                {
+                    if (actual.Datos.Año < siguiente.Datos.Año)
+                    {
+                        Carro temp = actual.Datos;
+                        actual.Datos = siguiente.Datos;
+                        siguiente.Datos = temp;
+                    }
+
+                    siguiente = siguiente.Siguiente;
+                }
+
+                actual = actual.Siguiente;
+            }
+        }
+
+    }
+
+
+
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Recuperacion_PHAC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListaEnlazada flota=new ListaEnlazada();
+            flota.AgregarCarro(new Carro(123,"Nissan",2023));
+            flota.AgregarCarro(new Carro(223, "Toyota",2022));
+            flota.AgregarCarro(new Carro(412, "Mustang",2018));
+            flota.AgregarCarro(new Carro(764, "BMW",2015));
+            flota.AgregarCarro(new Carro(669, "Ferrari",2017));
+
+            flota.Imprimirporaño();
+            flota.OrdenarPorAño();
+            Console.WriteLine($"Se encuentra el carro {flota.EncontrarCarro("Nissan")}");
+            flota.EncontrarCarro("Nissan");
+
+        }
+    }
+}
